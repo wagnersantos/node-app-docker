cced85a0e4f0a34ead9f96abf58f3a864ed46cbd
wagnersantos <wagtvc@gmail.com>
2020-01-28 22:17:00 -0300

    change production data

--- server/config.js
+++ server/config.js
@@ -6,8 +6,7 @@ const { Pool } = require("pg");
 const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 const production =
-  "postgres://etlwymluppaatp:347efbbe5f5b917ed93c310527e05ee48f5c2d43eb3d2e446768a484670025f6@ec2-174-129-33-139.compute-1.amazonaws.com:5432";
-
+  "postgres://etlwymluppaatp:347efbbe5f5b917ed93c310527e05ee48f5c2d43eb3d2e446768a484670025f6@ec2-174-129-33-139.compute-1.amazonaws.com:5432/d4ajsmvsk5g6oi";
 export const pool = new Pool({
   // connectionString: isProduction ? production : connectionString,
   connectionString: production,
