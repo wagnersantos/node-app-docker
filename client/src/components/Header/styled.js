da01c1e5f440c20535db2706d5b86f80f949644e
wagnersantos <wagtvc@gmail.com>
2020-01-29 22:28:22 -0300

    change height component header

--- client/src/components/Header/styled.js
+++ client/src/components/Header/styled.js
@@ -2,26 +2,29 @@ import styled from 'styled-components/native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 import { colors, metrics } from 'core/assets/styles';
+import { isIos } from 'environment';
+
+const headerHeight = isIos ? 54 : 25;
 
 export const ContentHome = styled.View`
-  padding: ${metrics.basePadding}px;
+  padding: ${metrics.basePadding / 2}px;
   flex-direction: row;
   align-items: flex-end;
   justify-content: center;
-  height: ${54 + getStatusBarHeight()}px;
+  height: ${headerHeight + getStatusBarHeight()}px;
   padding-top: ${getStatusBarHeight()}px;
-  background-color: ${({isDark}) => isDark ?  colors.white : colors.primary};
+  background-color: ${({ isDark }) => (isDark ? colors.white : colors.primary)};
   elevation: 1;
 `;
 
 export const Content = styled.View`
-  padding: ${metrics.basePadding}px;
+  padding: ${metrics.basePadding / 2}px;
   flex-direction: row;
   align-items: flex-end;
   justify-content: flex-start;
-  height: ${54 + getStatusBarHeight()}px;
+  height: ${headerHeight + getStatusBarHeight()}px;
   padding-top: ${getStatusBarHeight()}px;
-  background-color: ${({isDark}) => isDark ?  colors.white : colors.primary};
+  background-color: ${({ isDark }) => (isDark ? colors.white : colors.primary)};
   elevation: 1;
 `;
 
@@ -31,5 +34,5 @@ export const Title = styled.Text`
   flex-grow: 1;
   align-self: flex-end;
   text-align: center;
-  color: ${({isDark}) => isDark ?  colors.black : colors.white};
+  color: ${({ isDark }) => (isDark ? colors.black : colors.white)};
 `;
